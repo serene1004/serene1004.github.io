d2cb3989ccedfe18dc8c652e0667e54947e82a0f
serene1004 <vcbxnzn@gmail.com>
2025-06-13 11:21:04 +0900

    refactor: app 설정 관련 소스를 src/app 폴더로 정리

--- src/app/providers/router.js
+++ src/app/providers/router.js
@@ -27,10 +27,8 @@ const routes = [
   },
 ]
 
-const router = createRouter({
+export const router = createRouter({
   // history: createWebHistory(import.meta.env.BASE_URL),
   history: createWebHashHistory(),
   routes
 })
-
-export default router
--- src/main.js
+++ src/main.js
@@ -1,32 +1,3 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from '@/router'
-import { createPinia } from 'pinia'
-import './assets/reset.css'
-import 'pretendard/dist/web/static/pretendard.css'
+import { app } from "./app/index"
 
-import PrimeVue from 'primevue/config'
-import 'primeicons/primeicons.css'
-import Aura from '@primevue/themes/aura'
-import { definePreset } from '@primevue/themes'
-import { preset } from '@/shared/config/preset'
-
-const app = createApp(App)
-
-const MyPreset = definePreset(Aura, preset)
-
-app.use(PrimeVue, {
-  theme: {
-      preset: MyPreset,
-      options: {
-        prefix: 'p',
-        darkModeSelector: '.dark',
-        cssLayer: false
-      }  
-  },
-  ripple: true
-})
-
-app.use(createPinia())
-app.use(router)
-app.mount('#app')
+app.mount("#app")

81ba2f8ccde4c7154f67407f7aaf1de7bdfb1a88
serene1004 <vcbxnzn@naver.com>
2025-11-18 02:11:58 +0900

    feat: SolarSystem 수정 버전 배경 컴포넌트 추가

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="relative h-dvh w-full overflow-hidden bg-slate-100 dark:bg-slate-900 text-slate-100 dark:text-slate-50">
+  <div class="relative h-dvh w-full overflow-hidden">
+    <!-- 배경캔버스 -->
+    <BackgroundCanvas/>
     <!-- 데스크톱 영역 -->
-    <div class="flex h-full flex-col">
-      <main class="relative flex-1 overflow-hidden">
+    <div class="flex h-full flex-col text-slate-100 dark:text-slate-50">
+      <main class="flex-1 overflow-hidden">
         <slot />
       </main>
       <Footer/>
@@ -12,6 +14,9 @@
 
 
 <script setup lang="ts">
+import BackgroundCanvas from '~/components/BackgroundCanvas.vue'
+import Footer from '~/components/Footer.vue'
+
 const appConfig = useAppConfig()
 
 useHead({
